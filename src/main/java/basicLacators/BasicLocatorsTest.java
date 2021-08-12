f450826c81f9c2646d1686c1fc67a144b6df7406
Askar Musakunov <askarmusakunov@Askars-MacBook-Pro-2.local>
2021-08-11 20:59:31 -0500

    Basic Locators

---
+++ src/main/java/basicLacators/BasicLocatorsTest.java
@@ -0,0 +1,97 @@
+package basicLacators;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicLocatorsTest {
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Before
+    public void goToHomepage() {
+        //Instead of System.setProperty("driver.chrome", "Path")
+        driver.get("https://google.com");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void findById() {
+        //findElement is a method that looks up for an element in the page source
+        //by using different locators.
+        //there many different locators like By.ID, by.name etc.
+        WebElement feelingLuckyButton = driver.findElement(By.id("gbqfbb"));
+        Assert.assertEquals("Wrong text value on feelingLucky Button",
+                "I'm Feeling Lucky", feelingLuckyButton.getAttribute("value"));
+        //getText() -> return the text that's displayed on the webElement
+        //getAttribute(String atributeName);
+    }
+
+    @Test
+    public void findByName() {
+        WebElement searchButton = driver.findElement(By.name("btnK"));
+        Assert.assertEquals("Wrong text value on Search Button",
+                "Google Search", searchButton.getAttribute("value"));
+    }
+
+
+    @Test
+    public void findByLinkText() {
+        System.out.println("Navigated to google.com");
+        WebElement gmailLink = driver.findElement(By.linkText("Gmail"));
+        System.out.println("Located Gmail link");
+        Assert.assertEquals("Gmail link text is invalid", "Gmail", gmailLink.getText());
+        System.out.println("Assertion for link text passed");
+        gmailLink.click();
+        System.out.println("Clicked on gmail link");
+        //What is URL? -> webAddress
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertEquals("Gmail link on google.com is not taking to the right page",
+                "https://www.google.com/gmail/about/#", driver.getCurrentUrl());
+
+
+    }
+
+
+    @Test
+    public void partialLinkText() {
+        WebElement howSearchWorksLink = driver.findElement(By.partialLinkText("How Search"));
+        howSearchWorksLink.click();
+        Assert.assertEquals("Google Search - Discover How Google Search Works", driver.getTitle());
+    }
+
+    //NoSuchElementException ->
+    //checked vs unchecked ->
+
+
+    @Test
+    public void className() {
+
+        //When using className you have to make it's a unique classname used by just one element
+
+        WebElement youtubeShorts = driver.findElement(By.className("NKcBbd"));
+        youtubeShorts.click();
+        Assert.assertEquals("Introducing the shorter side of YouTube - YouTube", driver.getTitle());
+    }
+
+        //Go to https://demostore.x-cart.com/
+        //validate if the paypal icon with text is displaye or not?
+
+
+            //Go to https://demostore.x-cart.com/
+            //Click on Fashion
+            //
+}
--- src/main/resources/notes.md
+++ src/main/resources/notes.md
@@ -41,4 +41,40 @@ Every tag can have ID and names. This is super important attributes bc we will b
  a -> anchortext -> link
  
 
+###Different type of Apps.
+Web Apps -> You can access(use) this app only on a browser. 
+Desktop UI Apps -> Run on the personal computer directly. Microsoft Office, Zoom, Slack, Teams.
+Server Apps -> Runs on servers and has no UI. 
+Mobile Apps ->  Run on mobile phones.
 
+Web Apps vs Desktop App
+Web Apps are more flexible accessible from phones, any OS.
+Web App versions can be updated without the users' actions. 
+Web Apps dont have to be downloaded.
+
+Desktop Apps
+Can work offline. 
+More features. 
+More secure.
+Has access to Ram, Memory. 
+They can run with other programs.
+They can be programmed to run other kernel level commands.
+ 
+
+Selenium is good for Web Browser Functional Testing. 
+UI has a design. And Selenium is not a good tool to perform design testing. 
+
+Design - look and positioning of web elements.
+
+Can not handle captcha -> how do we test apps that have captcha. 
+We perform testing in QA enviroment. and we disable captcha in QA. 
+
+
+
+
+
+Java is enterprise level programming language.
+
+
+
+CRM -> Customer Relations Managment tool.
