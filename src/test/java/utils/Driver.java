a890d6a626c7e20dd6d5db334de2e6ea64bc671c
devxschool <60677263+devxschool@users.noreply.github.com>
2022-03-04 16:18:49 -0600

    Update Driver.java

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -37,7 +37,9 @@ public class Driver {
             switch (ConfigReader.getProperty("dbank.browser").toLowerCase()) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                     ChromeOptions options = new ChromeOptions();
+                    options.addArguments("headless");
+                    driver = new ChromeDriver(options);
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
