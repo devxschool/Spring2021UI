fda7efbba764f3788b44bde88ab86103957a433b
devxschool <60677263+devxschool@users.noreply.github.com>
2022-03-04 16:22:32 -0600

    Update Driver.java

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
 import java.io.IOException;
